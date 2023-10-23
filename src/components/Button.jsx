a44b3efdc304199fa097d1b1640b82aab1e8a546
VIJAYA BASKAR <vj2k02@gmail.com>
2023-10-23 20:57:57 +0530

    add basix routing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 const Button = (props) => {
   const varient = {
-    base: "bg-neutral50 text-neutral1000 w-w24 rounded-3xl text-center shadow-btnShadow backdrop-blur-2xl gap-2.5 px-6 py-2 hover:bg-neutral200",
-    outline: "",
+    base: "bg-neutral50 px-6 py-2  text-neutral1000 rounded-3xl text-center  font-bold shadow-btnShadow backdrop-blur-2xl gap-2.5 hover:bg-neutral200",
+    outline: "px-6 py-2  rounded-3xl shadow border border-twitterBlue border-opacity-25 backdrop-blur-2xl justify-center gap-2.5 items-center gap-2.5 inline-flex text-twitterBlue",
+    bluebtn: "bg-neutral50 px-6 py-2 text-neutral50 bg-twitterBlue rounded-3xl text-center  font-bold shadow-btnShadow backdrop-blur-2xl gap-2.5 hover:bg-neutral200 "
   };
-  const size = {
-    sm: "text-sm",
-    md: "text-sm",
-    lg: "text-sm",
-  };
+
+
+  const btnsize = {
+    sm: "w-24 text-sm",
+    md: "w-w24 text-md",
+    lg: "w-96 text-lg",
+  }
+
 
   const disableState = props.disable ? "disabled:opacity-50" : "";
 
-  const Class = `${varient[props.varient]} ${size[props.size]} ${disableState}`;
+  const Class = `${varient[props.varient]} ${btnsize[props.btnsize]} ${disableState}`;
   return (
     <>
       <button className={Class}>{props.text}</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Error404 from "./routes/Error404.jsx"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from "react-router-dom";
+import "./index.css";
+import Screen01 from "./routes/Screen01.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const route = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App />}>
+    <Route path="screen01" element={<Screen01 />} />
+  </Route>
+));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={route} />
+  </React.StrictMode>
+);
